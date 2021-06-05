d7f72c621381121e74dac4a31b080224b13c0819
William Derek Dias <williamdkdevops@gmail.com>
2021-06-05 15:51:24 -0300

    Changed component adjustment

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Room } from '../room';
-import { RoomService } from '../room.service';
 import { RoomDetailsComponent } from 'src/app/room-details/room-details.component';
+import { Observable } from "rxjs";
+import { RoomService } from "../room.service";
+import { Room } from "../room";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-room-list',
-  templateUrl: './room-list.component.html',
-  styleUrls: ['./room-list.component.css']
+  selector: "app-room-list",
+  templateUrl: "./room-list.component.html",
+  styleUrls: ["./room-list.component.css"]
 })
 export class RoomListComponent implements OnInit {
   rooms: Observable<Room[]>;
 
-  constructor(private roomService: RoomService, private router: Router) { }
+  constructor(private roomService: RoomService,
+    private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.reloadData();
   }
 
@@ -23,20 +24,21 @@ export class RoomListComponent implements OnInit {
     this.rooms = this.roomService.getRoomsList();
   }
 
-  deleteRoom (id : number) {
-    this.roomService.deleteRoom(id).subscribe(
-      data => {
-        console.log(data);
-        this.reloadData();
-      },
-      error => console.log(error));
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
   }
 
-  roomDetails(id:number) {
+  roomDetails(id: number){
     this.router.navigate(['details', id]);
   }
 
-  updateRoom(id:number) {
+  updateRoom(id: number){
     this.router.navigate(['update', id]);
   }
 }
